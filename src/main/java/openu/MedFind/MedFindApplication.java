837ac2bf74c0992b511d047872b24c256da27e15
Avinoam Malka <malka.avinoam@gmail.com>
2022-03-12 23:22:02 +0200

    Initializing project

---
+++ src/main/java/openu/MedFind/MedFindApplication.java
@@ -0,0 +1,13 @@
+package openu.MedFind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedFindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedFindApplication.class, args);
+	}
+
+}
