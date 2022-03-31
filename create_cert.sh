18f9150ab63aef3da6320b124931156a012ed5b5
Avinoam <malka.avinoam@gmail.com>
2022-03-31 10:37:45 +0300

    reverting some debugging

--- create_cert.sh
+++ create_cert.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -x
 
-echo "HERE!!!"
-/bin/ls -l $1
-echo $1
 $1 -noprompt -genkeypair -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore build/.keyStore/key.cert -validity 3650 -dname "CN=medfind.com, OU=Medfind, O=Medfind, L=Israel, S=Israel, C=IL" -keypass SecretP4ss -storepass SecretP4ss
