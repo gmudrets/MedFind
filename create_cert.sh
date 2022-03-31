01ad0250d341b855593754576974d617195dd048
Avinoam <malka.avinoam@gmail.com>
2022-03-31 10:29:39 +0300

    fixing Linux file

--- create_cert.sh
+++ create_cert.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
+/bin/ls -l $1
 echo $1
 $1 -noprompt -genkeypair -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore build/.keyStore/key.cert -validity 3650 -dname "CN=medfind.com, OU=Medfind, O=Medfind, L=Israel, S=Israel, C=IL" -keypass SecretP4ss -storepass SecretP4ss
