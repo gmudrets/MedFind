293c4a5a30782497fde7cb79b6764ee476d3b2e9
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-04-11 00:27:16 +0300

    Update RegistrationController.java

--- src/main/java/openu/MedFind/restservice/controllers/RegistrationController.java
+++ src/main/java/openu/MedFind/restservice/controllers/RegistrationController.java
@@ -5,14 +5,6 @@ import openu.MedFind.dto.RegistrationRequest;
 import openu.MedFind.restservice.services.RegistrationService;
 import org.springframework.web.bind.annotation.*;
 
-
-// TO-DO list Server side:
-// 1. validation of fields (?)
-// 2. check if username or email already exists.
-// 3. create new user object (DTO) and add it to the DB.
-// To read about: WebRequest, Model, Firebase, Threads.
-// 4. add threading (?)
-
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @AllArgsConstructor
