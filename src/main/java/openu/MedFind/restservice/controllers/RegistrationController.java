c176ec0ab2909fe6b700fbf452cdadbdc13ad121
IdoAbramov <59793426+IdoAbramov@users.noreply.github.com>
2022-04-12 21:25:40 +0300

    Update RegistrationController.java

--- src/main/java/openu/MedFind/restservice/controllers/RegistrationController.java
+++ src/main/java/openu/MedFind/restservice/controllers/RegistrationController.java
@@ -3,6 +3,7 @@ package openu.MedFind.restservice.controllers;
 import lombok.AllArgsConstructor;
 import openu.MedFind.dto.RegistrationRequest;
 import openu.MedFind.restservice.services.RegistrationService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -13,7 +14,7 @@ public class RegistrationController {
     private RegistrationService registrationService;
 
     @PostMapping("/api/User/Register")
-    public String UserRegistration(@RequestBody RegistrationRequest registerRequest){
+    public ResponseEntity<String> UserRegistration(@RequestBody RegistrationRequest registerRequest){
         return registrationService.registerNewUser(registerRequest);
     }
 }
