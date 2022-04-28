064d66b933d073bc2ca4ae7393cd5e65af66a062
NegevTag <wildnegev@gmail.com>
2022-04-28 11:41:27 +0300

    Finish time of the day handling

--- src/main/resources/static/client/src/Components/RemindersArea/RemindersCreateForm.js
+++ src/main/resources/static/client/src/Components/RemindersArea/RemindersCreateForm.js
@@ -1,6 +1,6 @@
 import {Box} from "@mui/material";
 import {getThemeProps} from "@mui/system";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -19,15 +19,18 @@ import AddAlarmIcon from '@mui/icons-material/AddAlarm';
 import IconButton from "@mui/material/IconButton";
 import Paper from "@mui/material/Paper";
 import {useNavigate} from "react-router-dom";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+import {AdapterDateFns} from "@mui/x-date-pickers/AdapterDateFns";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+import {TimePicker} from "@mui/x-date-pickers/TimePicker";
 
 function RemindersCreateForm(props) {
+    const maxTimes = 15;
     const [stopStream, setStopStream] = useState(false);
     const [timesArray, setTimesArray] = useState([null]);
     const [value, setValue] = React.useState(null);
-
+    const [hasErrorArr, setHasErrorArr] = React.useState(new Array(15).fill(false));
+    const [triedSubmit, setTriedSubmitted] = React.useState(false);
+    const [reachedMaxTimes, setReachedMaxTimes] = React.useState(false);
     const navigate = useNavigate();
 
     const types = [
@@ -35,6 +38,8 @@ function RemindersCreateForm(props) {
         'רופא',
         'צוות רפואי',
     ];
+
+
     const [userType, setUserType] = useState(types[0]);
 
     const handleSelectUserType = (event) => {
@@ -49,12 +54,53 @@ function RemindersCreateForm(props) {
             password: data.get('password'),
         });
     };
-    const handleTimeChange = (time, index) => {
+    const showError = (time, index) => {
+        let hasError = !validateTime(time, index);
 
+        if (!triedSubmit && time == null) {
+            return false;
+        } else {
+            return hasError;
+        }
+
+
+    }
+    const validateTime = (time, index) => {
+        if ((new Date(time) == "Invalid Date") || isNaN(new Date(time)) || Date.parse(new Date(time)) === 0) {
+            return false;
+        }
+
+        for (let i = 0; i < timesArray.length; i++) {
+            if (index != i) {
+                if (timesArray[index] == timesArray[i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    const checkIfHasError = (time,index) =>{
+        let hasError = !validateTime(time, index);
+        let nextHasError = [...hasErrorArr];
+        nextHasError[index] = hasError;
+        setHasErrorArr(nextHasError);
+    }
+    useEffect(() => {
+        if (timesArray.length == maxTimes) {
+            setReachedMaxTimes(true);
+        } else {
+            setReachedMaxTimes(false);
+        }
+    });
+    const handleTimeChange = (time, index) => {
+        const next = [...timesArray];
+        next[index] = time;
+        setTimesArray(next);
     }
     const handleAddTimeClick = () => {
         const next = [...timesArray, null];
         setTimesArray(next);
+
         console.log(next);
     }
     const dismissBarcodeReader = () => {
@@ -97,7 +143,7 @@ function RemindersCreateForm(props) {
                                 />
                             </Grid>
                             {timesArray.map((time, index) =>
-                                <Grid item xs={12}>
+                                <Grid item xs={12} key={"test123" + index}>
                                     <LocalizationProvider dateAdapter={AdapterDateFns}>
                                         <TimePicker
                                             label="Basic example"
@@ -105,14 +151,18 @@ function RemindersCreateForm(props) {
                                             onChange={(newValue) => {
                                                 handleTimeChange(newValue, index);
                                             }}
-                                            renderInput={(params) => <TextField {...params} />}
+                                            renderInput={(params) => <TextField {...params} fullWidth
+                                                                                error={showError(time, index)}
+                                                                                />}
+
                                         />
                                     </LocalizationProvider>
                                 </Grid>)
                             }
 
                             <Grid item xs={12}>
-                                <IconButton onClick={handleAddTimeClick}><AddAlarmIcon/></IconButton>
+                                <IconButton onClick={handleAddTimeClick}
+                                            disabled={reachedMaxTimes}><AddAlarmIcon/></IconButton>
                             </Grid>
 
                             <Grid item xs={12}>
