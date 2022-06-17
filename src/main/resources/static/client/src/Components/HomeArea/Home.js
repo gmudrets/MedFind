b88fa56b6780d8db90ed01e3a33adda094deefd0
Genadi Mudrets <gmudrets@gmail.com>
2022-06-17 22:30:56 +0300

    fix typo

--- src/main/resources/static/client/src/Components/HomeArea/Home.js
+++ src/main/resources/static/client/src/Components/HomeArea/Home.js
@@ -399,7 +399,7 @@ function Home() {
                       anchorOrigin={{vertical: 'top', horizontal: 'center'}}
             >
                 <Alert severity="error">
-                    אראה שגיאה בהוספת התרופה למאגר
+                    אירעה שגיאה בהוספת התרופה למאגר
                 </Alert>
             </Snackbar>
             { items.length > 0 ? (
