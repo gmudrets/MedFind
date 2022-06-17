bf41d7d41680c8bbb98325090e5c0b330cee9fb0
NegevTag <wildnegev@gmail.com>
2022-06-17 12:08:58 +0300

    no errors now

--- src/main/resources/static/client/craco.config.js
+++ src/main/resources/static/client/craco.config.js
@@ -3,14 +3,38 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 const path = require('path')
 
 module.exports = {
-
+    entry: "./app",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: 'node_modules',
+                loader: 'babel',
+                query: {presets: ['es2015']},
+            }
+        ]
+    },
+    target: 'node',
     webpack: {
         plugins: {
             add: [
                 new NodePolyfillPlugin()
             ]
         },
+        node:{}
+        // resolve: {
+        //     fallback: {
+        //         "crypto": false,
+        //         "stream": false
+        //
+        //     }
+        // }
 
     },
+
 
 }
