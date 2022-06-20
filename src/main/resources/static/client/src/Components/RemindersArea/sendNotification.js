a1c54ed23f3138fe9139d49a005f38e88896fe4d
NegevTag <wildnegev@gmail.com>
2022-06-21 00:35:28 +0300

    added send notification function

---
+++ src/main/resources/static/client/src/Components/RemindersArea/sendNotification.js
@@ -0,0 +1,25 @@
+export function sendNotification(header, content, uid) {
+    var url = "https://onesignal.com/api/v1/notifications";
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url);
+
+    xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+    xhr.setRequestHeader("Authorization", "Basic OTgxNzVjNTUtNTU3Yi00OTUyLThhYzMtMGY3ZjBhNjRlYjM3");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }
+    };
+
+    var data = `{"app_id": "577a3ade-c29c-434b-8c12-ee8749ed6dfb",
+    "contents": {"en":` + content + `},
+    "headings": {"en": ` + header + ` },
+    "channel_for_external_user_ids": "push",
+     "include_external_user_ids": ["` + uid + `"]}`;
+
+    xhr.send(data);
+
+}
