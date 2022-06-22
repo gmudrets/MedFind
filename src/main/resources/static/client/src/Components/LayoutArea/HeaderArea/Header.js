4002a6c2fcb38c2da2a9aec73fdf22ec57cf4768
NegevTag <wildnegev@gmail.com>
2022-06-22 11:46:04 +0300

    fix small bug

--- src/main/resources/static/client/src/Components/LayoutArea/HeaderArea/Header.js
+++ src/main/resources/static/client/src/Components/LayoutArea/HeaderArea/Header.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, {useState, useEffect} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import logo from '../../../Assets/Images/logo.png'
-import { getSafe } from '../../../Utils/Utils'
+import {getSafe} from '../../../Utils/Utils'
 import * as STATE_PATHS from '../../../Consts/StatePaths'
 import {Actions} from "../../../Redux/UI";
 import ReadSocket from "./ReadSocket";
@@ -17,89 +17,86 @@ import * as ProfileFields from "../../../Consts/ProfileFields";
 
 function Header() {
 //   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const [auth, setAuth] = useState(true);
-  const [anchorEl, setAnchorEl] = useState(null);
-  const currentUser = useSelector((state) => getSafe(STATE_PATHS.USER_DETAILS, state));
-  const isMenuOpen = useSelector((state) => getSafe(STATE_PATHS.SIDE_MENU_OPEN, state));
+    const dispatch = useDispatch();
+    const [auth, setAuth] = useState(true);
+    const [anchorEl, setAnchorEl] = useState(null);
+    const currentUser = useSelector((state) => getSafe(STATE_PATHS.USER_DETAILS, state));
+    const isMenuOpen = useSelector((state) => getSafe(STATE_PATHS.SIDE_MENU_OPEN, state));
 
-  const profile = useSelector((state) => getSafe(USER_PROFILE, state));
-  const fullName = profile[ProfileFields.FIRST_NAME] + " " + profile[ProfileFields.LAST_NAME];
-  const profilePicture = profile[ProfileFields.PROFILE_PICTURE];
+    const profile = useSelector((state) => getSafe(USER_PROFILE, state));
+    const fullName = profile[ProfileFields.FIRST_NAME] + " " + profile[ProfileFields.LAST_NAME];
+    const profilePicture = profile[ProfileFields.PROFILE_PICTURE];
 
-  useEffect(() => {
-    if (currentUser === ''){
-        setAuth(false);
+    useEffect(() => {
+        if (currentUser === '') {
+            setAuth(false);
+        } else {
+            setAuth(true);
+        }
+
+    }, [currentUser])
+
+    const toggleMenu = () => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        if (!isMenuOpen) {
+            dispatch(Actions.openMenu());
+        } else {
+            dispatch(Actions.closeMenu())
+        }
+    };
+    const readSocket = (data) => {
+        console.log(data.body);
+        if (data !== []) {
+            console.log(data.body);
+
+        }
     }
-    else {
-        setAuth(true);
-    }
-  
-  }, [currentUser])
+    return (
+        <Box sx={{flexGrow: 1}}>
+            <AppBar position="fixed" color="grey">
+                {auth &&
+                    <ReadSocket readSocket={readSocket}/>
+                }
+                <Toolbar>
+                    {auth && <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        onClick={toggleMenu()}
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>}
 
-  const toggleMenu = () => (event) => {
-    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-      return;
-    }
+                    <Typography component="div" sx={{flexGrow: 1}}>
+                        <a href="/"><img src={logo} className="Logo" alt="logo" style={{maxWidth: '115px'}}/></a>
+                    </Typography>
 
-    if (!isMenuOpen) {
-      dispatch(Actions.openMenu());
-    }
-    else {
-      dispatch(Actions.closeMenu())
-    }
-  };
-  const readSocket = (data) => {
-    console.log(data.body);
-    if (data !== []) {
-      console.log(data.body);
+                    {auth && (
+                        <Typography align={"center"} variant="subtitle2">
+                            שלום, {fullName}
+                            &nbsp;
+                        </Typography>)}
 
-    }
-  }
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed" color="grey">
-      {auth &&
-          <ReadSocket  readSocket={readSocket}/>
-      }
-      <AppBar position="static" color="grey">
-        <Toolbar>
-          {auth && <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            onClick={toggleMenu()}
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>}
-
-          <Typography component="div" sx={{ flexGrow: 1 }}>
-            <a href="/"><img src={logo} className="Logo" alt="logo" style={{ maxWidth: '115px' }} /></a>
-          </Typography>
-
-          {auth && (
-          <Typography align={"center"} variant="subtitle2">
-            שלום, {fullName}
-            &nbsp;
-          </Typography>)}
-
-          {auth && (
-              <a href="/#/settings"><img alt="profilePicture" src={profilePicture}
-                                         style={{
-                                           borderRadius: "50%",
-                                           maxWidth : "50px",
-                                           maxHeight : "75px",
-                                           minWidth : "30px",
-                                           minHeight : "45px",
-                                           objectFit : "cover"
-                                         }}/></a>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
-  );
+                    {auth && (
+                        <a href="/#/settings"><img alt="profilePicture" src={profilePicture}
+                                                   style={{
+                                                       borderRadius: "50%",
+                                                       maxWidth: "50px",
+                                                       maxHeight: "75px",
+                                                       minWidth: "30px",
+                                                       minHeight: "45px",
+                                                       objectFit: "cover"
+                                                   }}/></a>
+                    )}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
 
 export default Header;
