5e67c1a4c56583d3803c8e1702ae6f9a4bf0fc23
NegevTag <wildnegev@gmail.com>
2022-06-23 11:50:35 +0300

    fix so loading work and prepare closest notifications

--- src/main/resources/static/client/src/Components/HomeArea/UpcomingAlerts.js
+++ src/main/resources/static/client/src/Components/HomeArea/UpcomingAlerts.js
@@ -5,14 +5,45 @@ import Typography from "@mui/material/Typography";
 import {getRequest} from "../../Utils/AxiosRequests";
 import {getAuth} from "firebase/auth";
 import {ServerConsts} from "../../Consts/apiPaths";
+import {FIXED, FIXED_DATE, REM_TYPE} from "../../Consts/RemindersFields";
+
 
 function UpcomingAlerts() {
 
-    const [ alerts, setAlerts ] = useState([]);
+    const [alerts, setAlerts] = useState(null);
+    const showAlertNum = 5;
+    const findClosestAlerts = (alerts) => {
+        if (alerts !== null) {
+            const arr = [];
+            for (let i = 0; i < alerts.length; i++) {
+                if (alerts[i][REM_TYPE] === FIXED) {
+                    const timeDiff = new Date(alerts[i][FIXED_DATE]).getTime() - new Date().getTime();
+                    const inner = [timeDiff, alerts[i]];
+                    arr.push(inner);
+                }
+            }
+        } else {
+            return null;
+        }
 
+    }
     useEffect(async () => {
-        let alertsList = await getRequest(await getAuth().currentUser.getIdToken(true), ServerConsts.GET_USER_ALERT_LIST);
-        setAlerts(alertsList);
+        if (getAuth().currentUser !== null) {
+            const token = await getAuth().currentUser.getIdToken(true);
+            const list = await getRequest(token, ServerConsts.GET_USER_ALERT_LIST);
+            setAlerts(list);
+
+
+        } else {
+            setTimeout(async () => {
+                if (getAuth().currentUser !== null) {
+                    const token = await getAuth().currentUser.getIdToken(true);
+                    const list = await getRequest(token, ServerConsts.GET_USER_ALERT_LIST);
+                    setAlerts(list);
+                }
+            }, 1000)
+        }
+        ;
     }, []);
 
     return (
@@ -20,7 +51,7 @@ function UpcomingAlerts() {
             <Typography variant="h6" component="h2" align='center' marginTop={4}>
                 התראות קרובות
             </Typography>
-            {alerts.length > 0 ? (alerts.map((item,index) => (
+            {alerts !== null && alerts.length > 0 && (alerts.map((item, index) => (
                 <Box
                     key={index}
                     marginTop='65px'
@@ -32,11 +63,17 @@ function UpcomingAlerts() {
                 >
                     <DetailedCard type={"alert"} title={item.title} subheader={item.subheader} body={item.body}/>
                 </Box>
-            ))) : (
-                <Typography variant="subtitle1" component="h2" align='center'>
-                     אין התראות להצגה
+            )))}
+            {alerts === null &&
+                <Typography variant="subtitle1" component="h2" align='center' key={alerts === null ? "0" : "1"}>
+                    טוען התראות...
                 </Typography>
-            )}
+            }
+            {alerts.length === 0 &&
+                <Typography variant="subtitle1" component="h2" align='center'>
+                    אין התראות להצגה
+                </Typography>
+            }
         </>
     );
 
